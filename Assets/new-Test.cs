18926996bf105ee9afe1d305823be40f2123ba93
kantanvgat <kantan.vgat@gmail.com>
2025-05-20 16:10:32 +0900

    コミット

--- Assets/new-Test.cs
+++ Assets/new-Test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class new-Test : MonoBehaviour
+public class new- Test: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
